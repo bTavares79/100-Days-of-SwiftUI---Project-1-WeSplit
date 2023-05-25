345294557f5b0053e9de3af2b6b5720c944dcfd4
Brian Tavares <btavares79@gmail.com>
2023-05-25 08:08:27 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Brian Tavares on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
